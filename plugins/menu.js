bfa94b20a5245f9a8c8ccc8b615289678998f0df
Kri.com <107316046+KrizynOfc@users.noreply.github.com>
2022-07-20 02:56:46 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -207,7 +207,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
 }
 handler.help = ['allmenu']
 handler.tags = ['main']
-handler.command = /^(allmenu|\?)$/i
+handler.command = /^(allmenu)$/i
 handler.register = true
 handler.exp = 45
 
